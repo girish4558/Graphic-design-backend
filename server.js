16cf14220318f557e783a4c7c85a3de4309b46de
KalaiSakthi <sakthikalai003@gmail.com>
2026-02-14 15:27:34 +0530

    wo db be local 5000

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const connectDB = require('./config/db');
-connectDB();
 
-
- 
 const app = express();
- 
-connectDB();
- 
+
+/* Connect DB */
+// connectDB();
+
+/* Middleware */
 app.use(cors());
 app.use(express.json());
- 
+
+app.get("/", (req, res) => {
+  res.send("Backend is working without database!");
+});
+
+
+/* Routes */
 app.use("/api/auth", authRoutes);
- 
+
+/* Start server */
 app.listen(5000, () => console.log("Server running on port 5000"));
